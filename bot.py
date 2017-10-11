be7872a93908b0152da421666941fe765ed9d50c
fogside <cheskidova@phystech.edu>
2017-10-11 14:19:19 +0300

    refactor+feat: change stack structure to tree and add new buttons

--- bot.py
+++ bot.py
@@ -1,158 +1,203 @@
 # -*- coding: utf-8 -*-
-import time
 import telebot
-import os
 from telebot import types
 import config
-import requests
-from sqlighter import SQLighter
+# from sqlighter import SQLighter
+from collections import defaultdict
 
 bot = telebot.TeleBot(config.token)
-markup_main = types.ReplyKeyboardMarkup()
-markup_main.row('🦉 Задать вопрос к семинару 🦉')
-markup_main.row('🐌 Сдать домашку 🐌')
-markup_main.row('🐸 Зарегаться 🐸')
 
-markup_hw = types.ReplyKeyboardMarkup()
+markup_cleared = types.ReplyKeyboardRemove()
+
+markup_ask_again = types.ReplyKeyboardMarkup(resize_keyboard=True)
+markup_ask_again.row('Задать еще один вопрос')
+markup_ask_again.row('Вернуться назад')
+
+markup_back = types.ReplyKeyboardMarkup(resize_keyboard=True)
+markup_back.row('Вернуться назад')
+
+markup_course = types.ReplyKeyboardMarkup(resize_keyboard=True)
+markup_course.row('🐟 RL 🐟')
+markup_course.row('🐸 NLP 🐸')
+
+markup_hw_q = types.ReplyKeyboardMarkup(resize_keyboard=True)
+markup_hw_q.row('🦉 Задать вопрос к семинару 🦉')
+markup_hw_q.row('🐌 Сдать домашку 🐌')
+markup_hw_q.row('Вернуться в начало')
+
+markup_hw = types.ReplyKeyboardMarkup(resize_keyboard=True)
 for name in config.possible_to_pass:
     markup_hw.row(name)
-markup_hw.row('Вернуться в начало')
+markup_hw.row('Вернуться назад')
 
-FLAGS = {'question': False, 'hw': None, 'register': False}
-
-def drop_flags(current):
-    for k, val in FLAGS.items():
-        if (k != current):
-            if val is bool:
-                FLAGS[k] = False
-            else:
-                FLAGS[k] = None
-# start
-@bot.message_handler(commands=['start'])
-def handle_start_help(message):
-    bot.send_chat_action(message.chat.id, 'typing')
-    bot.send_message(message.chat.id, "Доброе время суток! Чего изволите?", reply_markup=markup_main)
-    drop_flags('all')
+states = defaultdict(dict)
 
 
-# Вернуться в начало'
-@bot.message_handler(func=lambda msg: msg.text == 'Вернуться в начало', content_types=['text'])
-def handle_start_help(message):
-    bot.send_chat_action(message.chat.id, 'typing')
-    bot.send_message(message.chat.id, "Чего еще изволите?", reply_markup=markup_main)
-    drop_flags('all')
+@bot.message_handler(content_types=['text', 'document', 'photo'])
+def handler(message):
+    global states
+    chat_id = message.chat.id
+    bot.send_chat_action(chat_id, 'typing')
+
+    if message.chat.username is None:
+        username_error(message)
+        return
+
+    elif message.chat.id not in states:
+        clear_state(message, states)
+        default_callback(message, states)
+
+    elif states[chat_id]['currentState'] == 'WAIT_USER_INTERACTION':
+        default_callback(message, states)
+
+    elif (states[chat_id]['currentState'] == 'COURSE_SELECTION') and (message.text in ['🐟 RL 🐟', '🐸 NLP 🐸']):
+        choose_question_or_hw(message)
+        states[chat_id]['currentState'] = 'HW_OR_QUESTION_SELECTION'
+        states[chat_id]['course'] = message.text.split()[1]
+
+    elif (states[chat_id]['currentState'] == 'HW_OR_QUESTION_SELECTION') and \
+            (message.text in ['🦉 Задать вопрос к семинару 🦉', '🐌 Сдать домашку 🐌']):
+        if message.text == '🦉 Задать вопрос к семинару 🦉':
+            question_handler(message)
+            states[chat_id]['currentState'] = 'IN_QUESTION'
+
+        elif message.text == '🐌 Сдать домашку 🐌':
+            hw_handler(message)
+            states[chat_id]['currentState'] = 'HW_NUM_SELECTION'
+
+    elif (states[chat_id]['currentState'] == 'IN_QUESTION') and \
+            (message.text != 'Вернуться в начало') and (message.content_type == 'text'):
+        if message.text == 'Вернуться назад':
+            states[chat_id]['currentState'] = 'HW_OR_QUESTION_SELECTION'
+            choose_question_or_hw(message)
+        elif message.text == 'Задать еще один вопрос':
+            question_handler(message)
+        else:
+            question_saver(message)
+
+    elif (states[chat_id]['currentState'] == 'HW_NUM_SELECTION') and (message.content_type == 'text') and \
+            ((message.text in config.possible_to_pass) or (message.text == 'Вернуться назад')):
+        if message.text in config.possible_to_pass:
+            states[chat_id]['currentState'] = 'IN_HW_UPLOAD'
+            states[chat_id]['hw_num'] = message.text
+            hw_waiter(message)
+        elif message.text == 'Вернуться назад':
+            states[chat_id]['currentState'] = 'HW_OR_QUESTION_SELECTION'
+            choose_question_or_hw(message)
+
+    elif (states[chat_id]['currentState'] == 'IN_HW_UPLOAD') and\
+            ((message.content_type in ['document', 'photo']) or message.text == 'Вернуться назад'):
+        if message.text == 'Вернуться назад':
+            states[chat_id]['currentState'] = 'HW_NUM_SELECTION'
+            hw_handler(message)
+        else:
+            hw_saver(message, states)
+    else:
+        print('DEFAULT')
+        default_callback(message, states)
+
+    print(states)
+
+
+# use this function as default handler
+def default_callback(message, states):
+    if message.text == '/start':
+        bot.send_message(message.chat.id,
+                         'WELCOME, *{}*!\nCHOOSE YOUR DESTINY!'.format(message.chat.username.upper()),
+                         reply_markup=markup_course, parse_mode='Markdown')
+        clear_state(message, states, set_state='COURSE_SELECTION')
+
+    elif message.text == '/help':
+        bot.send_message(message.chat.id,
+                         'Привет, {}!\nЯ умею принимать вопросы к семинарам и файлы с дз по курсам NLP & RL.\n'
+                         'Другие мои возможности находятся в разработке. Чтобы приступить к работе выберите курс.'
+                         .format(message.chat.username.title()),
+                         reply_markup=markup_course)
+
+    elif message.text == 'Вернуться в начало':
+        bot.send_message(message.chat.id,
+                         'HERE AGAIN, *{}*!\nCHOOSE YOUR DESTINY!'.format(message.chat.username.upper()),
+                         reply_markup=markup_course, parse_mode='Markdown')
+        clear_state(message, states, set_state='COURSE_SELECTION')
+
+    else:
+        bla_bla_detected(message)
+        clear_state(message, states)
+
+
+def clear_state(message, states, set_state='WAIT_USER_INTERACTION'):
+    states[message.chat.id]['currentState'] = set_state
+    states[message.chat.id]['course'] = None
+    states[message.chat.id]['hw_num'] = None
+
+
+def username_error(message):
+    bot.send_message(message.chat.id,
+                     'Для продолжения работы с ботом, пожалуйста, запилите себе username в настройках телеграм!',
+                     reply_markup=markup_cleared)
+
+
+def choose_question_or_hw(message):
+    bot.send_message(message.chat.id, 'Выберите доступное действие, пожалуйста 🦊', reply_markup=markup_hw_q)
+
 
 # Задать вопрос к семинару
-@bot.message_handler(func=lambda msg: msg.text == '🦉 Задать вопрос к семинару 🦉', content_types=['text'])
 def question_handler(message):
-    FLAGS['question'] = True
-    bot.send_chat_action(message.chat.id, 'typing')
-    bot.send_message(message.chat.id, "Спрашивайте, пожалуйста 🦊"
-                                      "\nВаш вопрос будет обсужден на семинаре.\n"
-                                      "Внимание: все нижесказанное cообщение (одно) будет воспринято как вопрос к семинаристу.")
-    drop_flags('question')
+    bot.send_message(message.chat.id, 'Сформулируйте вопрос к семинаристу и отправьте его одним сообщением 🦊.',
+                     reply_markup=markup_back)
+
 
 # Сдать домашку
-@bot.message_handler(func=lambda msg: msg.text == '🐌 Сдать домашку 🐌', content_types=['text'])
 def hw_handler(message):
-    bot.send_chat_action(message.chat.id, 'typing')
-    drop_flags('all')
-    sqlbd = SQLighter(config.bd_name)
-    if not sqlbd.is_registered(message.from_user.id):
-        sqlbd.close()
-        bot.send_message(message.chat.id, "Прежде чем сдавать домашку, зарегайтесь, пожалуйста.\n"
-                                          "Для этого достаточно нажать на кнопочку '🐸 Зарегаться 🐸'", reply_markup=markup_main)
-        return
-    bot.send_message(message.chat.id, "Пожалуйста выберите из списка доступных для сдачи заданий:",
+    bot.send_message(message.chat.id, 'Пожалуйста, выберите из списка доступных для сдачи заданий.',
                      reply_markup=markup_hw)
 
 
-# Зарегаться
-@bot.message_handler(func=lambda msg: msg.text == '🐸 Зарегаться 🐸', content_types=['text'])
-def register_handler(message):
-    bot.send_chat_action(message.chat.id, 'typing')
-
-    sqlbd = SQLighter(config.bd_name)
-    if sqlbd.is_registered(message.from_user.id):
-        drop_flags('all')
-        name = sqlbd.get_user_name(message.from_user.id)
-        sqlbd.close()
-        bot.send_message(message.chat.id, "Кажется вы уже зарегестрированы в системе под именем {} 🌵\n"
-                                          "К сожалению ничего изменить уже нельзя. Сдавайте домашки. 🌚".format(name))
-        return
-
-    FLAGS['register'] = True
-    drop_flags('register')
-    bot.send_message(message.chat.id, "В следующем сообщении напишите как вас называть. 🐝\n"
-                                      "Это имя будет использовано для дальнейшей идентификации вас при проверке дз.\n"
-                                      "Оно также будет привязано к вашему телеграм-аккаунту 🌚\n"
-                                      "Внимание! В дальнейшем изменить ваше имя будет невозможно 🐙")
 # hw -- выбор домашки
-@bot.message_handler(func=lambda msg: msg.text in config.possible_to_pass, content_types=['text'])
-def hw_saver(message):
-    sqlbd = SQLighter(config.bd_name)
-    if not sqlbd.is_registered(message.from_user.id):
-        sqlbd.close()
-        bot.send_message(message.chat.id, "Прежде чем сдавать домашку, зарегайтесь, пожалуйста.\n"
-                                          "Для этого достаточно нажать на кнопочку '🐸 Зарегаться 🐸'", reply_markup=markup_main)
-        return
-    FLAGS['hw'] = message.text
-    drop_flags('hw')
-    bot.send_chat_action(message.chat.id, 'typing')
-    bot.send_message(message.chat.id, "Пришлите файл (один архив или один Jupyter notebook) весом не более 20 Мб 🦋",
-                     reply_markup=markup_hw)
+def hw_waiter(message):
+    bot.send_message(message.chat.id, 'Пришлите файл **(один архив или один Jupyter notebook)** весом не более 20 Мб.',
+                     reply_markup=markup_back, parse_mode='Markdown')
+
 
 # hw -- сохранение файла домашки
-@bot.message_handler(func=lambda msg: FLAGS['hw'] is not None, content_types=['document'])
-def handle_docs(message):
-    bot.send_chat_action(message.chat.id, 'typing')
+def hw_saver(message, states):
+    username = message.chat.username
 
-    file_id = message.document.file_id
-    sqlbd = SQLighter(config.bd_name)
-    sqlbd.add_homework(message.from_user.id, FLAGS['hw'], file_id=file_id)
-    folder_name = os.path.join(config.SAVE_PATH, str(message.from_user.id))
-    # saving to folder:
-    if not os.path.exists(folder_name):
-        os.mkdir(folder_name)
-    file_info = bot.get_file(file_id)
-    print(file_info.file_path)
-    file = requests.get('https://api.telegram.org/file/bot{0}/{1}'.format(config.token, file_info.file_path), stream=True)
-    local_filename = os.path.join(folder_name, FLAGS['hw']+'_'+message.document.file_name)
-    with open(local_filename, 'wb') as f:
-        for chunk in file.iter_content(chunk_size=1024):
-            if chunk: # filter out keep-alive new chunks
-                f.write(chunk)
-    bot.send_message(message.chat.id, "Ваш файлик был заботливо сохранен как задание {} 🐾".format(FLAGS['hw']),
-                     reply_markup=markup_hw)
-    drop_flags('all')
+    if message.content_type == 'photo':
+        bot.send_message(message.chat.id, "{}, красивая картиночка, но она не похожа на домашку 🚫\n"
+                                          "Наши преподаватели предпочитают следующие расширения: {}.\n"
+                                          "Напоминаю, что дз сдается в виде одного файла архива или одного Jupyter ноутбука."
+                         .format(username.title(), str(config.available_hw_resolutions)))
+        return
 
-# register -- ловим имя пользователя
-@bot.message_handler(func=lambda msg: FLAGS['register'], content_types=['text'])
-def register_saver(message):
-    bot.send_chat_action(message.chat.id, 'typing')
-    drop_flags('all')
-    sqlbd = SQLighter(config.bd_name)
-    user_id = message.from_user.id
-    sqlbd.register(user_id=user_id, user_name=message.text)
-    sqlbd.close()
-    bot.send_message(message.chat.id, "Спасибо! Теперь вы зарегистрированы в системе как {}".format(message.text),
-                     reply_markup=markup_main)
+    hw_num = states[message.chat.id]['hw_num']
+    filename = message.document.file_name
+
+    if not filename.endswith(config.available_hw_resolutions):
+        bot.send_message(message.chat.id, "🚫 {}, очень жаль но файлик не сдается в нашу систему...\n"
+                                          "Наши преподаватели предпочитают следующие расширения: {}.\n"
+                                          "Напоминаю, что дз сдается в виде одного файла архива или одного Jupyter ноутбука."
+                         .format(username.title(), str(config.available_hw_resolutions)))
+        return
+
+    bot.send_message(message.chat.id, 'Уважаемый *{}*, ваш файлик был заботливо сохранен как задание {} 🐾\n'
+                     .format(username.title(), hw_num),
+                     reply_markup=markup_hw, parse_mode='Markdown')
+    clear_state(message, states, set_state='HW_NUM_SELECTION')
 
 
 # Сохранение вопроса к семинару
-@bot.message_handler(func=lambda msg: FLAGS['question'], content_types=['text'])
 def question_saver(message):
-    drop_flags('all')
-    bot.send_chat_action(message.chat.id, 'typing')
-    sqlbd = SQLighter(config.bd_name)
-    user_id = message.from_user.id
-    sqlbd.write_question(user_id=user_id, question=message.text)
-    sqlbd.close()
-    bot.send_message(message.chat.id, "Спасибо за вопрос. Хорошего дня 🐯 :)")
+    bot.send_message(message.chat.id, 'Спасибо за вопрос. Хорошего дня 🐯 :)\n'
+                                      'Нажмите кнопку, если желаете задать еще один вопрос.',
+                     reply_markup=markup_ask_again)
 
-@bot.message_handler(func=lambda msg: True, content_types=['text'])
-def question_saver(message):
-    bot.send_message(message.chat.id, "Кажется вы мне говорите какую-то дичь :)")
+
+def bla_bla_detected(message):
+    bot.send_message(message.chat.id, 'Я вас не понимаю.\n'
+                                      'Нажмите /start чтобы начать жизнь с чистого листа ☘️',
+                     reply_markup=markup_cleared)
+
 
 if __name__ == '__main__':
     bot.polling(none_stop=True)

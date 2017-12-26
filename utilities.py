91e66652d63a94efeab81027ad7ac51dfffaddf4
fogside <cheskidova@phystech.edu>
2017-12-26 16:09:38 +0300

    feat: added mkdir

--- utilities.py
+++ utilities.py
@@ -6,10 +6,12 @@ import telebot
 
 def download_file(bot, file_id, folder_name, filename):
     file_info = bot.get_file(file_id)
-    print(file_info.file_path)
     file = requests.get('https://api.telegram.org/file/bot{0}/{1}'.format(config.token, file_info.file_path),
                         stream=True)
     local_filename = os.path.join(folder_name, filename)
+    if not os.path.exists(folder_name):
+        os.mkdir(folder_name)
+
     with open(local_filename, 'wb') as f:
         for chunk in file.iter_content(chunk_size=1024):
             if chunk:  # filter out keep-alive new chunks
